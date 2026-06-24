203fae531d38c462bd868f682399625980add9c2
hgjulin7-tech <hgjulin7@gmail.com>
2026-06-25 05:18:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function showMessage() {
-  alert("Welcome to Hungama Esports!");
+function register() {
+    window.open(
+        "https://wa.me/919817289390?text=Hi%20I%20want%20to%20register%20for%20the%20BGMI%20Tournament",
+        "_blank"
+    );
 }
